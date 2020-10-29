8549e63ca3a9bc9880a84ba1bbe113ed4816d0e3
Hanna Aspblad <hanna.aspblad@iths.se>
2020-10-29 21:55:24 +0100

    Update main.js
    test to create list element with fetched data

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,8 @@
 
+const bodyElement = document.querySelector("body")
+const unorderedList= document.createElement("ul")
+bodyElement.append(unorderedList)
+
 async function getData(){
 
 const charactersRequest = await fetch("https://swapi.dev/api/people/")
@@ -9,6 +13,18 @@ const planets = await planetsRequest.json()
 
 console.log(characters.results)
 console.log(planets.results)
+
+const characterName = document.createElement("li")
+unorderedList.append(characterName)
+characterName.innerHTML = characters.results[0].name
+
+const characterHeight = document.createElement("li")
+unorderedList.append(characterHeight)
+characterHeight.innerHTML = characters.results[0].height
+
+const characterMass = document.createElement("li")
+unorderedList.append(characterMass)
+characterMass.innerHTML = characters.results[0].mass
 
 }
 
