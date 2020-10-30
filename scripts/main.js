185d1061bef5467268e8b173e0825bf85cf5a471
Hanna Aspblad <hanna.aspblad@iths.se>
2020-10-30 21:39:07 +0100

    list detail function
    fixed so the detail list + render work except for that the list needs to be cleared

--- scripts/main.js
+++ scripts/main.js
@@ -2,135 +2,108 @@
 getData()
 listCharacters()
 
-
-
-
 async function getData() {
-
-    
-    
 
     const charactersRequest = await fetch("https://swapi.dev/api/people/")
     const data = await charactersRequest.json()
 
     return data.results
-    
 }
-
 
 async function listCharacters() {
 
-
     displayLoader()
+
     const characters = await getData()
 
+    hideLoader()
 
 
     for (const currentCharacter of characters) {
 
         renderCharacterList(currentCharacter)
-        
+
     }
-
-    
-
 }
 
 
-async function listDetails(){
-
-    const characterDetails = await getData()
-
-    
-
-    for(i=0; i < characterDetails.length; i++){
-        renderDetails(characterDetails)
-
-    }
-
-    
-
-
-}
-
-
-
-function renderCharacterList(input) {
+function renderCharacterList(character) {
 
     const characterList = document.querySelector(".character-list")
     const characterSlot = document.createElement("li")
     characterList.append(characterSlot)
 
-
-
-    characterSlot.innerHTML = input.name
+    characterSlot.innerHTML = character.name
 
     characterSlot.addEventListener("click", function (event) {
 
-       
         const targetedCharacter = event.target.innerText
-        console.log(targetedCharacter)
-        //index of targeted character -> skicka in som val i arrayen i render details (i = index of targeted character)
-        // const res = characters.indexof(targetedCharacter)
+        listDetails(targetedCharacter)
 
-        // console.log(res)
-       // renderDetails()
-        
     })
 
 }
 
 
-function renderDetails(input){
+async function listDetails(character) {
 
+    let char = []
+
+    const list = await getData()
+
+
+    for (const currentCharacter of list) {
+
+        if (currentCharacter.name == character) {
+
+            char = currentCharacter
+        }
+
+    } renderDetails(char)
+}
+
+
+function renderDetails(character) {
+
+    console.log(character)
 
     const characterDetails = document.querySelector(".character-details")
     const characterName = document.querySelector(".character-name")
-   
-    characterName.innerHTML = input[i].name
+
+    characterName.innerHTML = character.name
 
     const characterHeight = document.createElement("li")
     characterDetails.append(characterHeight)
-    characterHeight.innerHTML = "Heigh: " + input[i].height
+    characterHeight.innerHTML = "Heigh: " + character.height
 
     const characterMass = document.createElement("li")
     characterDetails.append(characterMass)
-    characterMass.innerHTML = "Mass: " + input[i].mass
+    characterMass.innerHTML = "Mass: " + character.mass
 
     const characterHairColor = document.createElement("li")
     characterDetails.append(characterHairColor)
-    characterHairColor.innerHTML = "Hair color: " + input[i].hair_color
+    characterHairColor.innerHTML = "Hair color: " + character.hair_color
 
     const characterSkinColor = document.createElement("li")
     characterDetails.append(characterSkinColor)
-    characterSkinColor.innerHTML = "Skin color: " + input[i].skin_color
+    characterSkinColor.innerHTML = "Skin color: " + character.skin_color
 
     const characterEyeColor = document.createElement("li")
     characterDetails.append(characterEyeColor)
-    characterEyeColor.innerHTML = "Eye color: " + input[i].eye_color
-    
+    characterEyeColor.innerHTML = "Eye color: " + character.eye_color
 
     const characterBirthYear = document.createElement("li")
     characterDetails.append(characterBirthYear)
-    characterBirthYear.innerHTML = "Eye color: " + input[i].birth_year
+    characterBirthYear.innerHTML = "Eye color: " + character.birth_year
 
     const characterGender = document.createElement("li")
     characterDetails.append(characterGender)
-    characterGender.innerHTML = "Gender: " + input[i].gender
-
-    
-
-
-
-    
-    
+    characterGender.innerHTML = "Gender: " + character.gender
 
 }
 
 
-
-
-function displayLoader(){
+function displayLoader() {
 
     const bodyElement = document.querySelector("body")
     const divElement = document.createElement("div")
@@ -141,9 +114,10 @@ function displayLoader(){
 }
 
 
-function hideLoader(loader) {
+function hideLoader() {
 
-        loader.classList.add("hide-loader")
+    const loader = document.querySelector(".loader")
+    loader.classList.add("hide-loader")
 
 }
 
