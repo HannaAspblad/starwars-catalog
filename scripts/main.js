132d4900bfe27042546d6bd8d8b5969b2d49e244
AbbasMansoori <70722051+AbbasMansoori@users.noreply.github.com>
2020-11-04 00:37:51 +0100

    nu rensas listan på "click"
    la till functionen så charlist och planetinfo rensas vid click.

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,14 @@
-
-getCharacterData()
-
+getCharacterData();
 
 async function getCharacterData() {
+  const charactersRequest = await fetch("https://swapi.dev/api/people/");
+  const characterData = await charactersRequest.json();
 
-    const charactersRequest = await fetch("https://swapi.dev/api/people/")
-    const characterData = await charactersRequest.json()
-
-    return characterData.results
+  return characterData.results;
 }
 
-
 async function listCharacters() {
+  const loaderPosition = document.querySelector(".left");
 
     const loaderPosition = document.querySelector(".character")
     
@@ -22,167 +19,143 @@ displayLoader(loaderPosition)
 
     for (const currentCharacter of characters) {
 
-        renderCharacterList(currentCharacter)
+  const characters = await getCharacterData();
+  hideLoader();
 
-    }
+  for (const currentCharacter of characters) {
+    renderCharacterList(currentCharacter);
+  }
 }
 
-listCharacters()
+listCharacters();
 
 function renderCharacterList(character) {
+  const characterList = document.querySelector(".character");
+  const characterSlot = document.createElement("li");
+  characterList.append(characterSlot);
 
-    const characterList = document.querySelector(".character")
-    const characterSlot = document.createElement("li")
-    characterList.append(characterSlot)
+  characterSlot.innerHTML = character.name;
 
-    characterSlot.innerHTML = character.name
-
-    characterSlot.addEventListener("click", function (event) {
-
-        const targetedCharacter = event.target.innerText
-        listCharacterDetails(targetedCharacter)
-
-    })
-
+  characterSlot.addEventListener("click", function (event) {
+    const targetedCharacter = event.target.innerText;
+    listCharacterDetails(targetedCharacter);
+  });
 }
-
 
 async function listCharacterDetails(character) {
+  const loaderPosition = document.querySelector(".charinfo");
 
-    const loaderPosition = document.querySelector(".charinfo")
+  displayLoader(loaderPosition);
 
-    displayLoader(loaderPosition)
+  let char = [];
+  const list = await getCharacterData();
 
-    let char = []
-    const list = await getCharacterData()
+  hideLoader();
 
-    hideLoader()
-
-    for (const currentCharacter of list) {
-
-        if (currentCharacter.name == character) {
-
-            char = currentCharacter
-        }
-
+  for (const currentCharacter of list) {
+    if (currentCharacter.name == character) {
+      char = currentCharacter;
     }
+  }
 
-    renderCharacterDetails(char)
-    listPlanetDetails(char)
+  renderCharacterDetails(char);
+  listPlanetDetails(char);
 }
 
-
 function renderCharacterDetails(character) {
+  let clear = document.getElementsByClassName("charinfo")[0];
+  clear.innerHTML = "";
+  const characterDetails = document.querySelector(".charinfo");
+  const characterName = document.querySelector(".charactername");
 
-    const characterDetails = document.querySelector(".charinfo")
-    const characterName = document.querySelector(".charactername")
+  characterName.innerHTML = character.name;
 
-    characterName.innerHTML = character.name
+  const characterHeight = document.createElement("li");
+  characterDetails.append(characterHeight);
+  characterHeight.innerHTML = "Heigh: " + character.height;
 
-    const characterHeight = document.createElement("li")
-    characterDetails.append(characterHeight)
-    characterHeight.innerHTML = "Heigh: " + character.height
+  const characterMass = document.createElement("li");
+  characterDetails.append(characterMass);
+  characterMass.innerHTML = "Mass: " + character.mass;
 
-    const characterMass = document.createElement("li")
-    characterDetails.append(characterMass)
-    characterMass.innerHTML = "Mass: " + character.mass
+  const characterHairColor = document.createElement("li");
+  characterDetails.append(characterHairColor);
+  characterHairColor.innerHTML = "Hair color: " + character.hair_color;
 
-    const characterHairColor = document.createElement("li")
-    characterDetails.append(characterHairColor)
-    characterHairColor.innerHTML = "Hair color: " + character.hair_color
+  const characterSkinColor = document.createElement("li");
+  characterDetails.append(characterSkinColor);
+  characterSkinColor.innerHTML = "Skin color: " + character.skin_color;
 
-    const characterSkinColor = document.createElement("li")
-    characterDetails.append(characterSkinColor)
-    characterSkinColor.innerHTML = "Skin color: " + character.skin_color
+  const characterEyeColor = document.createElement("li");
+  characterDetails.append(characterEyeColor);
+  characterEyeColor.innerHTML = "Eye color: " + character.eye_color;
 
-    const characterEyeColor = document.createElement("li")
-    characterDetails.append(characterEyeColor)
-    characterEyeColor.innerHTML = "Eye color: " + character.eye_color
+  const characterBirthYear = document.createElement("li");
+  characterDetails.append(characterBirthYear);
+  characterBirthYear.innerHTML = "Eye color: " + character.birth_year;
 
-    const characterBirthYear = document.createElement("li")
-    characterDetails.append(characterBirthYear)
-    characterBirthYear.innerHTML = "Eye color: " + character.birth_year
-
-    const characterGender = document.createElement("li")
-    characterDetails.append(characterGender)
-    characterGender.innerHTML = "Gender: " + character.gender
-
+  const characterGender = document.createElement("li");
+  characterDetails.append(characterGender);
+  characterGender.innerHTML = "Gender: " + character.gender;
 }
 
 async function listPlanetDetails(character) {
+  let clear = document.getElementsByClassName("planetinfo")[0];
+  clear.innerHTML = "";
 
-    const loaderPosition = document.querySelector(".planetinfo")
+  const loaderPosition = document.querySelector(".planetinfo");
 
-    displayLoader(loaderPosition)
+  displayLoader(loaderPosition);
 
-    const planetInfo = await fetch(character.homeworld)
-    const planetData = await planetInfo.json()
+  const planetInfo = await fetch(character.homeworld);
+  const planetData = await planetInfo.json();
 
-    hideLoader()
+  hideLoader();
 
-    renderPlanetDetails(planetData)
+  renderPlanetDetails(planetData);
 }
 
 function renderPlanetDetails(planet) {
+  const planetDetails = document.querySelector(".planetinfo");
+  const planetName = document.querySelector(".planetname");
 
-    const planetDetails = document.querySelector(".planetinfo")
-    const planetName = document.querySelector(".planetname")
+  planetName.innerHTML = planet.name;
 
-    planetName.innerHTML = planet.name
+  const planetRotation = document.createElement("li");
+  planetDetails.append(planetRotation);
+  planetRotation.innerHTML = "Rotation period: " + planet.rotation_period;
 
-    const planetRotation = document.createElement("li")
-    planetDetails.append(planetRotation)
-    planetRotation.innerHTML = "Rotation period: " + planet.rotation_period
+  const planetOrbital = document.createElement("li");
+  planetDetails.append(planetOrbital);
+  planetOrbital.innerHTML = "Orbital period: " + planet.orbital_period;
 
-    const planetOrbital = document.createElement("li")
-    planetDetails.append(planetOrbital)
-    planetOrbital.innerHTML = "Orbital period: " + planet.orbital_period
+  const planetDiameter = document.createElement("li");
+  planetDetails.append(planetDiameter);
+  planetDiameter.innerHTML = "Diameter: " + planet.diameter;
 
-    const planetDiameter = document.createElement("li")
-    planetDetails.append(planetDiameter)
-    planetDiameter.innerHTML = "Diameter: " + planet.diameter
+  const planetClimate = document.createElement("li");
+  planetDetails.append(planetClimate);
+  planetClimate.innerHTML = "Diameter: " + planet.climate;
 
-    const planetClimate = document.createElement("li")
-    planetDetails.append(planetClimate)
-    planetClimate.innerHTML = "Diameter: " + planet.climate
+  const planetGravity = document.createElement("li");
+  planetDetails.append(planetGravity);
+  planetGravity.innerHTML = "Gravity: " + planet.gravity;
 
-    const planetGravity = document.createElement("li")
-    planetDetails.append(planetGravity)
-    planetGravity.innerHTML = "Gravity: " + planet.gravity
-
-    const planetTerrain = document.createElement("li")
-    planetDetails.append(planetTerrain)
-    planetTerrain.innerHTML = "Terrain: " + planet.terrain
-
+  const planetTerrain = document.createElement("li");
+  planetDetails.append(planetTerrain);
+  planetTerrain.innerHTML = "Terrain: " + planet.terrain;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-function displayLoader(position){
-
-    const divElement = document.createElement("div")
-    const loader = divElement
-    loader.classList.add("loader")
-    position.append(loader)
-    
-    
-    }
+function displayLoader(position) {
+  const divElement = document.createElement("div");
+  const loader = divElement;
+  loader.classList.add("loader");
+  position.append(loader);
+}
 
 function hideLoader() {
-
-
-    const loader = document.querySelector(".loader")
-    loader.remove()
-
+  const loader = document.querySelector(".loader");
+  loader.remove();
 }
 
 pagination()
@@ -197,40 +170,4 @@ function pagination(){
 
 
 
-// --------------clear detailstest -----------------
 
-
-
-
-
-// function clearDetails() {
-
-//     const detailsLists = document.querySelector(".characterinfo .planetinfo")
-//     detailsLists.classList.add("clearinfo")
-
-// }
-
-// function showDetails() {
-//     const detailsLists = document.querySelector(".characterinfo .planetinfo")
-//     detailsLists.classList.add("clearinfo")
-
-// }
-
-
-
-
-
-
-// ----------------- test ------------------------- 
-
-// let detailsLists = document.querySelector('.planetinfo'),
-//     //detailsLists = document.querySelectorAll('li')
-
-// detailsLists.forEach(function(item){
-//     item.addEventListener('click', function(){
-//         tabLink.forEach(function(detailsLists) {
-//           detailsLists.classList.add('showplanetinfo')
-//         })
-//         item.classList.add('')
-//     }, false)
-// })
